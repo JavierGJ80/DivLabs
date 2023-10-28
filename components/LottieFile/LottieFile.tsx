858d17949649c6b45d9b11324bec74c4e92bcecf
JavierGJ80 <cabojavo@hotmail.com>
2023-10-27 20:12:44 -0600

    Final lottie

--- components/LottieFile/LottieFile.tsx
+++ components/LottieFile/LottieFile.tsx
@@ -1,23 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie";
-//import animation from "./lotties/Intento lottie.json"
+import animation from "./lotties/lottie final.json"
 import { LottieFileProps } from "./LottieFile.Types";
 
 const LottieFile = (props: LottieFileProps) => {
     const { fileName, width, height } = props
-    const [animation, setAnimation] = useState(null)
     const options = {
         loop: true,
         autoplay: true,
         animationData: animation
     };
-
-    useEffect(()=>{
-        fetch(`https://raw.githubusercontent.com/JavierGJ80/DivLabs/main/components/LottieFile/lotties/${fileName}.json`)
-            .then(response => {return response.json();} )
-            .then(jsonData => {setAnimation(jsonData);})
-            .catch(error => {console.error('Error while loading lottie:', error);})
-    }, [fileName])
 
     return(
         <div id="LottieContainer">
